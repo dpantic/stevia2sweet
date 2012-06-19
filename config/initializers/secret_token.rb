d8c94fbed3cf6121ee8a4aedd019269575d402e9
dpantic <dpantic@gmail.com>
2012-06-19 17:47:03 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stevia2sweet::Application.config.secret_token = '14f0c0ebcb8867342a27deade081ba2cf1ef8f1db459ffa0c90091a7ee6ac7ee0d45c520f3dcec45b66b1f2e5bd2411134f8fa23f9f56e966317faaeacb0b5de'
